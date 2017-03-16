78529f5e6399f4df0d6e7b8c8b9eacd240494dae
teramotodaiki <i101323@gmail.com>
2017-03-16 16:21:48 +0900

    decoder

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import {inspect} from 'util';
+
+
+const [,,source, output] = process.argv;
+if (!source || !output) {
+  throw new TypeError('Parameter was omitted. node unpack/index.js :source :output');
+}
+
+const sourcePath = path.resolve(source);
+const outputPath = path.resolve(output);
+
+console.log(`Feeles/unpack: ${sourcePath} into ${outputPath}`);
+
+mkdirIfNotExist(outputPath);
+
+const content = fs.readFileSync(sourcePath, 'utf8');
+const array = JSON.parse(content);
+for (const {name, composed} of array) {
+
+  const filePath = path.join(outputPath, name);
+  const loc = path.parse(filePath);
+  mkdirIfNotExist(loc.dir);
+  fs.writeFile(filePath, composed, 'base64');
+
+}
+
+function mkdirIfNotExist(absolutePath) {
+  if (!fs.existsSync(absolutePath)) {
+    const dir = path.parse(absolutePath).dir;
+    mkdirIfNotExist(dir);
+    fs.mkdir(absolutePath);
+  }
+}
